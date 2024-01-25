84683be729bd97d5aadd5dfc06560326165793e8
Henrique da Silva <86864451+HenriHTTP@users.noreply.github.com>
2024-01-25 20:14:53 -0300

    feat: create create user controller

---
+++ usecases/user/create_user_controller.py
@@ -0,0 +1,45 @@
+from entities.user import User
+from usecases.user.create_user_use_case import CreateUserUseCase
+from providers.http_response import HttpResponse
+
+
+class CreateUserController:
+    def __init__(self, create_user_use_case: CreateUserUseCase):
+        self.create_user_use_case = create_user_use_case
+
+    async def create_user(self, http_request: User):
+        required_fields = ['name', 'lastname', 'username', 'email', 'password']
+        null_fields = [field for field in required_fields if getattr(http_request, field) is None]
+        if null_fields:
+            http_response = HttpResponse(
+                success=False,
+                message="User not created",
+                error=f"{', '.join(null_fields)} is required",
+                status_code=500
+            )
+            return http_response
+        try:
+            http_request = User(
+                username=http_request.username,
+                email=http_request.email,
+                password=http_request.password,
+                lastname=http_request.lastname,
+                name=http_request.name
+            )
+            await self.create_user_use_case.execute(http_request)
+
+            http_response = HttpResponse(
+                success=True,
+                message="User created successfully",
+                error="no errors",
+                status_code=200
+            )
+            return http_response
+        except Exception as error:
+            http_response = HttpResponse(
+                success=False,
+                message="User not created",
+                error=str(error),
+                status_code=500
+            )
+            return http_response
