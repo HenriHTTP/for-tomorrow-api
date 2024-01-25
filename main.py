d53f53ef7f1fcac220615b90eb919d47cee9919a
Henrique da Silva <86864451+HenriHTTP@users.noreply.github.com>
2024-01-25 20:16:57 -0300

    refactor: implement blueprint

--- main.py
+++ main.py
@@ -1,13 +1,9 @@
-from  flask  import Flask 
+from flask import Flask
+from routes.create_user_rote import create_user_app
 
 app = Flask(__name__)
 
-
-@app.route('/index')
-def GetIndex (): 
-    return "page index"
-
-
+app.register_blueprint(create_user_app)
 
 if __name__ == '__main__':
-     app.run(debug=True)
+    app.run(debug=True)
