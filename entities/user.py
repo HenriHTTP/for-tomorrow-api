3eaf975a6f44336e368b50b55cfc784eb20b79d3
Henrique da Silva <86864451+HenriHTTP@users.noreply.github.com>
2024-01-25 20:11:19 -0300

    feat: create user entities

---
+++ entities/user.py
@@ -0,0 +1,10 @@
+class User:
+    def __init__(self, name, lastname, username, email, password):
+        self.name = name
+        self.lastname = lastname
+        self.username = username
+        self.email = email
+        self.password = password
+
+
+
