38a875ff90bd144e97781de6d6e4e678499534a9
Henrique da Silva <86864451+HenriHTTP@users.noreply.github.com>
2024-01-26 13:04:40 -0300

    refactor: remove arrow return type

--- repository/interface_user_repository.py
+++ repository/interface_user_repository.py
@@ -4,25 +4,25 @@ from entities.user import User
 
 class InterfaceUserRepository(ABC):
     @abstractmethod
-    async def get_user_by_email(self, email: str) -> User:
+    async def get_user_by_email(self, email: str):
         pass
 
     @abstractmethod
-    async def get_user_by_username(self, username: str) -> User:
+    async def get_user_by_username(self, username: str):
         pass
 
     @abstractmethod
-    async def get_user_by_id(self, user_id: any) -> User:
+    async def get_user_by_id(self, user_id: any):
         pass
 
     @abstractmethod
-    async def create_user(self, user: User) -> User:
+    async def create_user(self, user: User):
         pass
 
     @abstractmethod
-    async def update_user(self, user: User) -> User:
+    async def update_user(self, user: User):
         pass
 
     @abstractmethod
-    async def delete_user(self) -> bool:
+    async def delete_user(self):
         pass
