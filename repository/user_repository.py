17ef0b830984e830956dd1fc2798fad7fcf737e1
Henrique da Silva <86864451+HenriHTTP@users.noreply.github.com>
2024-01-26 13:06:38 -0300

    feat: implement database methods

--- repository/user_repository.py
+++ repository/user_repository.py
@@ -1,10 +1,14 @@
+from pymongo.database import Database
+
 from repository.interface_user_repository import InterfaceUserRepository
 from entities.user import User
 
 
 class UserRepository(InterfaceUserRepository):
-    def __init__(self, connection):
+    def __init__(self, connection, database, collection):
         self.connection = connection
+        self.database = database
+        self.collection = collection
 
     async def get_user_by_email(self, email: str):
         pass
@@ -12,22 +16,22 @@ class UserRepository(InterfaceUserRepository):
     async def get_user_by_username(self, username: str):
         pass
 
-    async def get_user_by_id(self, user_id: any) -> User:
+    async def get_user_by_id(self, user_id: any):
         pass
 
-    async def create_user(self, user: User) -> dict:
-        create_user = {
+    async def create_user(self, user: User):
+        document = {
             "email": user.email,
             "username": user.username,
             "password": user.password,
             "name": user.name,
             "lastname": user.lastname
         }
-        print(create_user)
-        return create_user
+        self.collection.insert_one(document)
+        return document
 
-    async def update_user(self, user: User) -> User:
+    async def update_user(self, user: User):
         pass
 
-    async def delete_user(self) -> bool:
+    async def delete_user(self):
         pass
