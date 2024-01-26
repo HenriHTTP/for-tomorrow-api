aedf63170518f377e89f2192256c1bf303693fc6
Henrique da Silva <86864451+HenriHTTP@users.noreply.github.com>
2024-01-26 13:06:05 -0300

    feat: create encrypt

---
+++ services/encrypt.py
@@ -0,0 +1,16 @@
+from hashlib import sha256
+from services.interface_encrypt import InterfaceEncrypt
+
+
+class Encrypt(InterfaceEncrypt):
+    def __init__(self, data):
+        self.data = data
+
+    def encrypt_data(self):
+        self.data = sha256(self.data.encode()).hexdigest()
+        return self.data
+
+    def validate_data(self, data: str):
+        if sha256(data.encode()).hexdigest() == self.data:
+            return True
+        return False
