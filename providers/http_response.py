d997730ca7fd5c1294fc794f74d0960d58c191ea
Henrique da Silva <86864451+HenriHTTP@users.noreply.github.com>
2024-01-29 13:33:15 -0300

    refactor: implement dataclass to http response

--- providers/http_response.py
+++ providers/http_response.py
@@ -1,6 +1,9 @@
+from dataclasses import dataclass
+
+
+@dataclass
 class HttpResponse:
-    def __init__(self, success, message, error, status_code):
-        self.success = success
-        self.message = message
-        self.error = error
-        self.status_code = status_code
+    success: bool
+    message: str
+    error: str
+    status_code: int
