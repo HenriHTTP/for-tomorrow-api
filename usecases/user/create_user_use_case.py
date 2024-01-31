d49dad5b340615b4738c4fe7a4c709a17797e38a
Henrique da Silva <86864451+HenriHTTP@users.noreply.github.com>
2024-01-31 15:06:08 -0300

    feat: create email is already used and username is already used

--- usecases/user/create_user_use_case.py
+++ usecases/user/create_user_use_case.py
@@ -9,22 +9,28 @@ class CreateUserUseCase:
 
     async def execute(self, user: User):
         try:
-            find_email_user = await self.user_repository.get_user_by_email(email=user.email)
-            is_used_email = True if len(find_email_user) >= 1 else False
-            if not is_used_email:
-                await self.user_repository.create_user(user)
-                return {
-                    "message": "User created successfully",
-                    "status_code": 200,
-                    "error": "no error on user creation",
-                    "success": True
-                }
-            else:
-                return {
-                    "message": "email is already used",
-                    "status_code": 500,
-                    "error": "email is already used, please try again",
-                    "success": False
-                }
+            await self.__email_is_already_used(user)
+            await self.__username_is_already_used(user)
+            await self.user_repository.create_user(user)
+            return {
+                "message": "User created successfully",
+                "status_code": 200,
+                "error": "no error on user creation",
+                "success": True
+            }
         except Exception as error:
             raise error
+
+    async def __email_is_already_used(self, user: User):
+        find_email_user = await self.user_repository.get_user_by_email(email=user.email)
+        is_used_email = True if len(find_email_user) >= 1 else False
+        if is_used_email:
+            error = "email is already used, please try again"
+            raise ValueError(error)
+
+    async def __username_is_already_used(self, user: User):
+        find_username_user = await self.user_repository.get_user_by_username(username=user.username)
+        is_used_username = True if len(find_username_user) >= 1 else False
+        if is_used_username:
+            error = "username is already used, please try again"
+            raise ValueError(error)
