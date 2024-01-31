b99a8a38764e4db94ac4ae5638dcfae7c32870a7
Henrique da Silva <86864451+HenriHTTP@users.noreply.github.com>
2024-01-30 23:09:49 -0300

    feat: implement return dict, is_used_email on create user

--- usecases/user/create_user_use_case.py
+++ usecases/user/create_user_use_case.py
@@ -9,6 +9,22 @@ class CreateUserUseCase:
 
     async def execute(self, user: User):
         try:
-            await self.user_repository.create_user(user)
+            find_email_user = await self.user_repository.get_user_by_email(email=user.email)
+            is_used_email = True if len(find_email_user) >= 1 else False
+            if not is_used_email:
+                await self.user_repository.create_user(user)
+                return {
+                    "message": "User created successfully",
+                    "status_code": 200,
+                    "error": "no error on user creation",
+                    "success": True
+                }
+            else:
+                return {
+                    "message": "email is already used",
+                    "status_code": 500,
+                    "error": "email is already used, please try again",
+                    "success": False
+                }
         except Exception as error:
             raise error
