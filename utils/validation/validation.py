922f57284b2c2c5421f7622b6d6c5d3aeee1ddf7
Henrique da Silva <86864451+HenriHTTP@users.noreply.github.com>
2024-02-01 19:36:57 -0300

    feat: create validation

---
+++ utils/validation/validation.py
@@ -0,0 +1,29 @@
+from services.email.email import Email
+from services.password.password import Password
+from entities.user import User
+
+
+class Validation:
+    @staticmethod
+    def validate_required_fields(http_request: User):
+        required_fields = ['name', 'lastname', 'username', 'email', 'password']
+        null_fields = [field for field in required_fields if getattr(http_request, field) is None]
+        if null_fields:
+            error_message = f"{', '.join(null_fields)} is required, please check the details"
+            raise ValueError(error_message)
+
+    @staticmethod
+    def validate_email(http_request: User):
+        email = Email(http_request.email)
+        if not email.is_valid():
+            error_message = f"{http_request.email} is not a valid email, please check the details"
+            raise ValueError(error_message)
+
+    @staticmethod
+    def validate_password(http_request: User):
+        password = http_request.password
+        validate_password = Password(password)
+        if not validate_password.is_valid_password():
+            error_message = ('invalid password, the password must not contain spaces, '
+                             'must contain digits, uppercase and lowercase letters')
+            raise ValueError(error_message)
