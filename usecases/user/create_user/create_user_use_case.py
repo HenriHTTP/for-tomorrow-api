fdc7f663798af223aefd453a135dddad2050ef09
Henrique da Silva <86864451+HenriHTTP@users.noreply.github.com>
2024-02-02 16:56:40 -0300

    refactor: implement private member user repository

--- usecases/user/create_user/create_user_use_case.py
+++ usecases/user/create_user/create_user_use_case.py
@@ -1,17 +1,16 @@
-from entities.user import User
 from repository.user_repository import UserRepository
 from entities.user import User
 
 
 class CreateUserUseCase:
     def __init__(self, user_repository: UserRepository):
-        self.user_repository = user_repository
+        self.__user_repository = user_repository
 
     async def execute(self, user: User):
         try:
             await self.__email_is_already_used(user)
             await self.__username_is_already_used(user)
-            await self.user_repository.create_user(user)
+            await self.__user_repository.create_user(user)
             return {
                 "message": "User created successfully",
                 "status_code": 200,
@@ -22,14 +21,14 @@ class CreateUserUseCase:
             raise error
 
     async def __email_is_already_used(self, user: User):
-        find_email_user = await self.user_repository.get_user_by_email(email=user.email)
+        find_email_user = await self.__user_repository.get_user_by_email(email=user.email)
         is_used_email = True if len(find_email_user) >= 1 else False
         if is_used_email:
             error = "email is already used, please try again"
             raise ValueError(error)
 
     async def __username_is_already_used(self, user: User):
-        find_username_user = await self.user_repository.get_user_by_username(username=user.username)
+        find_username_user = await self.__user_repository.get_user_by_username(username=user.username)
         is_used_username = True if len(find_username_user) >= 1 else False
         if is_used_username:
             error = "username is already used, please try again"
