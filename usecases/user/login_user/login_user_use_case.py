ba874fad606f79abc929047b3f76fdba08312a95
Henrique da Silva <86864451+HenriHTTP@users.noreply.github.com>
2024-02-13 18:35:00 -0300

    refactor: implement JWT token

--- usecases/user/login_user/login_user_use_case.py
+++ usecases/user/login_user/login_user_use_case.py
@@ -1,6 +1,7 @@
 from repository.user_repository import UserRepository
 from entities.user import User
 from services.encrypt.encrypt import Encrypt
+from services.token.token import Token
 
 
 class LoginUserUseCase:
@@ -13,13 +14,23 @@ class LoginUserUseCase:
             password_encrypt = Encrypt(user.password)
             user_database = email_user[0]
             user_password = user_database["password"]
+            user_id = str(user_database["_id"])
+            username = user_database["username"]
             is_valid_password = password_encrypt.validate_data(user_password)
+            token = Token(
+                user_id=user_id,
+                username=username
+            )
+            token.create_token()
+            user_token = token.get_token()
+            print(user_token)
             if is_valid_password:
                 return {
                     "message": "login completed successfully",
                     "status_code": 200,
                     "error": None,
-                    "success": True
+                    "success": True,
+                    "token": user_token
                 }
 
         except Exception as error:
