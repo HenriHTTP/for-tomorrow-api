27ae36a8a256f7371d4daaa452de88fd0c1a016e
Henrique da Silva <86864451+HenriHTTP@users.noreply.github.com>
2024-02-26 19:04:02 -0300

    refactor: implement data class to user entities

--- entities/user.py
+++ entities/user.py
@@ -1,10 +1,10 @@
+from dataclasses import dataclass
+
+
+@dataclass
 class User:
-    def __init__(self, name, lastname, username, email, password):
-        self.name = name
-        self.lastname = lastname
-        self.username = username
-        self.email = email
-        self.password = password
-
-
-
+    name: str
+    lastname: str
+    username: str
+    email: str
+    password: str
