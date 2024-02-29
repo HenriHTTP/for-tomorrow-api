b64eb850e6426775d55a17e51f5df18d126c99b0
Henrique da Silva <86864451+HenriHTTP@users.noreply.github.com>
2024-02-29 20:05:00 -0300

    feat: create create job controller

---
+++ usecases/job/create_job/create_job_controller.py
@@ -0,0 +1,43 @@
+from entities.job import Job
+from usecases.job.create_job.create_job_use_case import CreateJobUseCase
+from providers.http_response import HttpResponse
+from usecases.job.create_job.create_job_use_case import CreateJobUseCase
+from utils.validation.validation import Validation
+
+
+class CreateJobController:
+    def __init__(self, create_job_use_case: CreateJobUseCase):
+        self.__create_job_use_case = create_job_use_case
+
+    async def create_job(self, http_request: Job):
+        required_fields = [
+            'job_title',
+            'company',
+            'about_company',
+            'localization',
+            'job_type',
+            'job_description',
+            'requirements'
+        ]
+        try:
+            Validation.validate_required_fields(required_fields, http_request)
+            job = Job(
+                job_title=http_request.job_title,
+                company=http_request.company,
+                about_company=http_request.about_company,
+                localization=http_request.localization,
+                job_type=http_request.job_type,
+                job_description=http_request.job_description,
+                requirements=http_request.requirements
+            )
+            create_job = await self.__create_job_use_case.execute(job)
+            http_response = HttpResponse(
+                success=create_job["success"],
+                message=create_job["message"],
+                error=create_job["error"],
+                status_code=create_job["status_code"],
+                token=str(None)
+            )
+            return http_response
+        except Exception as error:
+            raise ValueError(error)
