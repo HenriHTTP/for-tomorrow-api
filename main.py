1f50c636f3ad0556ca8b4f5586c5f1ff183d2c23
Henrique da Silva <86864451+HenriHTTP@users.noreply.github.com>
2024-02-29 20:08:04 -0300

    feat: implement create job route

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 from flask import Flask
-from routes.create_user_rote import create_user_app
-from routes.login_user_route import login_user_app
+from routes.user.create_user_route import create_user_app
+from routes.user.login_user_route import login_user_app
+from routes.job.create_job_route import create_job_app
 
 app = Flask(__name__)
 
 app.register_blueprint(create_user_app)
 app.register_blueprint(login_user_app)
+app.register_blueprint(create_job_app)
 
 if __name__ == '__main__':
     app.run(debug=True)
