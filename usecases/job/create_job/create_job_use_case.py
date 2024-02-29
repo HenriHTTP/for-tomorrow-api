a067c00344ca3d5549f48dc497e65a911e6e4961
Henrique da Silva <86864451+HenriHTTP@users.noreply.github.com>
2024-02-29 20:04:47 -0300

    feat: create create job use cases

---
+++ usecases/job/create_job/create_job_use_case.py
@@ -0,0 +1,19 @@
+from repository.job.job_repository import JobRepository
+from entities.job import Job
+
+
+class CreateJobUseCase:
+    def __init__(self, job_repository: JobRepository):
+        self.job_repository = job_repository
+
+    async def execute(self, job: Job):
+        try:
+            await self.job_repository.insert_job(job)
+            return {
+                "message": "job created successfully",
+                "status_code": 200,
+                "error": None,
+                "success": True
+            }
+        except Exception as error:
+            raise ValueError(error)
