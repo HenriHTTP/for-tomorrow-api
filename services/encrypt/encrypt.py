46ec21ff0c479b2120224a3a4542eb2bac2b41d3
Henrique da Silva <86864451+HenriHTTP@users.noreply.github.com>
2024-03-08 00:43:24 -0300

    feat: implement boolean condition

--- services/encrypt/encrypt.py
+++ services/encrypt/encrypt.py
@@ -6,12 +6,13 @@ class Encrypt(InterfaceEncrypt):
     def __init__(self, data):
         self.data = data
 
-    def encrypt_data(self):
-        self.data = sha256(self.data.encode()).hexdigest()
+    def encrypt_data(self) -> str:
+        self.data: str = sha256(self.data.encode()).hexdigest()
         return self.data
 
-    def validate_data(self, data: str):
-        encrypted_data = sha256(self.data.encode()).hexdigest()
-        if data == encrypted_data:
+    def validate_data(self, data: str) -> bool:
+        encrypted_data: str = sha256(self.data.encode()).hexdigest()
+        is_valid_encrypted_data: bool = data == encrypted_data
+        if is_valid_encrypted_data:
             return True
         return False
